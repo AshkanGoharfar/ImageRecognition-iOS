d4632f16c75c10a2361a41275b17fc9e18832464
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-03-26 13:42:50 -0400

    ImageRecognition algorithm was implemented using CoreML.

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -24,9 +24,31 @@ class ViewController: UIViewController {
         let imagePath = Bundle.main.path(forResource: "car", ofType: "png")
         
         let imageURL = NSURL.fileURL(withPath: imagePath!)
+        
+        let modelFile = MobileNetV2()
+        
+        let model = try! VNCoreMLModel(for: modelFile.model)
+
+        let handler = VNImageRequestHandler(url: imageURL)
+        let request = VNCoreMLRequest(model: model, completionHandler: findResults)
+        try! handler.perform([request])
         // Do any additional setup after loading the view.
     }
 
-    
+    func findResults(request: VNRequest, error: Error?) {
+       guard let results = request.results as?
+       [VNClassificationObservation] else {
+       fatalError("Unable to get results")
+       }
+       var bestGuess = ""
+       var bestConfidence: VNConfidence = 0
+       for classification in results {
+          if (classification.confidence > bestConfidence) {
+             bestConfidence = classification.confidence
+             bestGuess = classification.identifier
+          }
+       }
+       labelDescription.text = "Image is: \(bestGuess) with confidence \(bestConfidence) out of 1"
+    }
 }
 
