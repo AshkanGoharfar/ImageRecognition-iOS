32f9b8b5506ad56fd12f20e6b000d055aa3f7a5c
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-03-26 13:33:59 -0400

    Initial commit

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var labelDescription: UILabel!
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let imagePath = Bundle.main.path(forResource: "car", ofType: "png")
+        
+        let imageURL = NSURL.fileURL(withPath: imagePath!)
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
